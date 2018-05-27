93ca5c348639b1292647293375cbc63d8faff11d
zouyang2015 <86022310@qq.com>
2018-05-27 13:47:10 +0800

    f

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -77,17 +77,21 @@ module.exports = __webpack_require__(1);
 "use strict";
 
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+__webpack_require__(2);
 
-var Test = function Test() {
-  _classCallCheck(this, Test);
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
 
-  this.a = 'hello222';
-};
+"use strict";
 
-var test = new Test();
 
-document.body.innerHTML = test.a;
+function test() {
+  var a = 1;
+  console.log(a);
+}
+
+test();
 
 /***/ })
 /******/ ]);
